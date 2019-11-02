a0fd031f4b6f330c3a85597f9bdd98d1e59c8555
jrf115 <jrf115@zips.uakron.edu>
2019-11-02 15:58:53 -0400

    Refactor and initialize menu functions

--- p3_5276.py
+++ p3_5276.py
@@ -4,28 +4,40 @@
 #   Project 3              #
 ############################
 
+def bayesian_Classifier():
 
-def py_nb():
+
+def test_bayesian_Classifier():
+
+def apply_bayesian_Classifier():
+
+# This is the top menu of the program
+def menu_1():
     print("1. Learn a Naive Bayesian classifierfrom data")
     print("2. Load and test accuracy of a naive Bayesian classifier.")
     print("3. Apply a naive Bayesian classifierto new cases.")
     print("4. Quit")
     selection = input()
-    while (selection == '1' or selection == '2' or selection == '3' or selection == '4') is false:
+    while selection != '1' and selection != '2' and selection != '3' and selection != '4':
         print("Please enter 1 2 or 3 only")
         selection = input()
-    if selection == 1:
-        print("Enter the filename of input data consisting of attributes and training examples in ARFF (Weka) format")
 
-    elif selection == 2:
-        print("Enter a model file saved previously")
-        modelFile = input()
+    if selection == '1':
+       bayesian_Classifier()
 
-    elif selection == 3:
-        print()
+    elif selection == '2':
+        test_bayesian_Classifier()
+
+    elif selection == '3':
+        apply_bayesian_Classifier()
 
     else:
         exit()
+
+
+# We are required to use py_nb() as the function to execute our program
+def py_nb():
+    menu_1()
 
 
 def main():
